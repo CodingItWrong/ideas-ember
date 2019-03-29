175d1648e692a3517315201184d6f02121f4efa4
Josh Justice <me@codingitwrong.com>
2019-03-29 14:26:59 -0400

    Add Ideas content

--- app/router.js
+++ app/router.js
@@ -6,6 +6,11 @@ const Router = EmberRouter.extend({
   rootURL: config.rootURL,
 });
 
-Router.map(function() {});
+Router.map(function() {
+  this.route('ideas', { path: '/ideas/:id' });
+  this.route('sources', function() {
+    this.route('detail', { path: '/:id' });
+  });
+});
 
 export default Router;
--- config/environment.js
+++ config/environment.js
@@ -33,6 +33,8 @@ module.exports = function(environment) {
     // ENV.APP.LOG_TRANSITIONS = true;
     // ENV.APP.LOG_TRANSITIONS_INTERNAL = true;
     // ENV.APP.LOG_VIEW_LOOKUPS = true;
+
+    ENV.apiHost = 'http://localhost:3000';
   }
 
   if (environment === 'test') {
