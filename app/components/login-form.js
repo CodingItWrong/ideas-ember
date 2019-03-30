5ce7b93a74af13091ac2afcca0f20b4af5a49de7
Josh Justice <me@codingitwrong.com>
2019-03-30 11:45:05 -0400

    Add login form

---
+++ app/components/login-form.js
@@ -0,0 +1,27 @@
+import Component from '@glimmer/component';
+import { action } from '@ember/object';
+import { tracked } from '@glimmer/tracking';
+import { inject as service } from '@ember/service';
+
+export default class LoginFormComponent extends Component {
+  @service session;
+  @service router;
+
+  email = 'example@example.com';
+  password = 'password';
+
+  @tracked
+  errorMessage = '';
+
+  @action
+  async logIn() {
+    let { email, password } = this;
+
+    try {
+      await this.session.authenticate('authenticator:oauth', email, password);
+      this.router.transitionTo('index');
+    } catch (e) {
+      this.errorMessage = e.error || e;
+    }
+  }
+}
