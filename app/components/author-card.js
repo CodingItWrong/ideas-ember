93b18e88fd9a8e935e1c7d090466db85cbdd7f52
Josh Justice <me@codingitwrong.com>
2019-03-30 08:05:44 -0400

    Delete author

--- app/components/author-card.js
+++ app/components/author-card.js
@@ -1,4 +1,15 @@
 import Component from '@glimmer/component';
+import { action } from '@ember/object';
 
 export default class AuthorCardComponent extends Component {
+  @action
+  delete() {
+    this.args.author.destroyRecord().catch(error => {
+      console.error(error);
+      alert(
+        'An error occurred while trying to delete this author.' +
+          ' Make sure there are no sources associated with her/him and try again.',
+      );
+    });
+  }
 }
