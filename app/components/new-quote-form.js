cb705ae78273836a0c7f945d0ff3eb846a2cf4bb
Josh Justice <me@codingitwrong.com>
2019-04-01 06:51:03 -0400

    Remove unnecessary ideas from new quote form

--- app/components/new-quote-form.js
+++ app/components/new-quote-form.js
@@ -9,20 +9,15 @@ export default class NewQuoteFormComponent extends Component {
   @tracked
   text = '';
 
-  @tracked
-  idea = null;
-
   @action
   async createQuote() {
     const quote = this.store.createRecord('quote', {
       source: this.args.source,
-      idea: this.idea,
       text: this.text,
     });
     try {
       await quote.save();
       this.text = '';
-      this.idea = null;
     } catch (e) {
       alert('An error occurred while saving this quote.');
       console.error(e);
