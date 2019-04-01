083f941e8c63c1809dfc8aef904c657ceca5a991
Josh Justice <me@codingitwrong.com>
2019-04-01 05:52:31 -0400

    Add idea to existing quote

--- app/components/quote-card.js
+++ app/components/quote-card.js
@@ -1,7 +1,23 @@
 import Component from '@glimmer/component';
 import { action } from '@ember/object';
+import { tracked } from '@glimmer/tracking';
 
 export default class QuoteCardComponent extends Component {
+  @tracked
+  addingIdea = false;
+
+  @tracked
+  idea = null;
+
+  @action
+  async saveIdea() {
+    const { quote } = this.args;
+    quote.set('idea', this.idea);
+    await quote.save();
+    this.idea = null;
+    this.addingIdea = false;
+  }
+
   @action
   delete() {
     if (!confirm('Are you sure you want to delete this quote?')) {
