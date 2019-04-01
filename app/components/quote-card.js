b3eb29364ce1e5cb855ecbba47b1505bcd892a82
Josh Justice <me@codingitwrong.com>
2019-04-01 06:19:56 -0400

    Add new idea to quote

--- app/components/quote-card.js
+++ app/components/quote-card.js
@@ -1,19 +1,54 @@
 import Component from '@glimmer/component';
 import { action } from '@ember/object';
 import { tracked } from '@glimmer/tracking';
+import { inject as service } from '@ember/service';
 
 export default class QuoteCardComponent extends Component {
+  @service store;
+
   @tracked
   addingIdea = false;
 
   @tracked
+  addingNewIdea = false;
+
+  @tracked
   idea = null;
+
+  @tracked
+  newIdeaSummary = '';
+
+  @action
+  switchToNewIdea() {
+    this.addingNewIdea = true;
+    this.idea = null;
+  }
+
+  @action
+  switchToExistingIdea() {
+    this.addingNewIdea = false;
+    this.newIdeaSummary = '';
+  }
 
   @action
   async saveIdea() {
     const { quote } = this.args;
-    quote.set('idea', this.idea);
+
+    let idea;
+
+    if (this.idea && !this.newIdeaSummary) {
+      idea = this.idea;
+    } else if (this.newIdeaSummary && !this.idea) {
+      idea = this.store.createRecord('idea', { summary: this.newIdeaSummary });
+      await idea.save();
+    } else {
+      alert('Either choose an existing idea or enter a new idea.');
+      return;
+    }
+
+    quote.set('idea', idea);
     await quote.save();
+
     this.idea = null;
     this.addingIdea = false;
   }
