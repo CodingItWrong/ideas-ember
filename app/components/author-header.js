64c6fb777929fbcfa46d9a0466a013d2f51184ad
Josh Justice <me@codingitwrong.com>
2019-04-22 07:14:10 -0400

    Allow editing author name

--- app/components/author-header.js
+++ app/components/author-header.js
@@ -1,4 +1,19 @@
 import Component from '@glimmer/component';
+import { tracked } from '@glimmer/tracking';
+import { action } from '@ember/object';
 
 export default class AuthorHeaderComponent extends Component {
+  @tracked
+  editing = false;
+
+  @tracked
+  editedAuthorName = this.args.author.name;
+
+  @action
+  async save() {
+    const { author } = this.args;
+    author.set('name', this.editedAuthorName);
+    await author.save();
+    this.editing = false;
+  }
 }
