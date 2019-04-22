4e8260ddc77b454ed8b62d14f2228e0e05a3fa66
Josh Justice <me@codingitwrong.com>
2019-04-22 17:16:06 -0400

    Move deleting from idea list to detail

--- app/components/idea-header.js
+++ app/components/idea-header.js
@@ -1,8 +1,11 @@
 import Component from '@glimmer/component';
 import { tracked } from '@glimmer/tracking';
 import { action } from '@ember/object';
+import { inject as service } from '@ember/service';
 
 export default class IdeaHeaderComponent extends Component {
+  @service router;
+
   @tracked
   editing = false;
 
@@ -26,5 +29,25 @@ export default class IdeaHeaderComponent extends Component {
     idea.set('summary', this.editedIdeaSummary);
     await idea.save();
     this.editing = false;
+  }
+
+  @action
+  delete() {
+    if (!confirm('Are you sure you want to delete this idea?')) {
+      return;
+    }
+
+    this.args.idea
+      .destroyRecord()
+      .then(() => {
+        this.router.transitionTo('index');
+      })
+      .catch(error => {
+        console.error(error);
+        alert(
+          'An error occurred while trying to delete this idea.' +
+            ' Make sure there are no quotes associated with it and try again.',
+        );
+      });
   }
 }
--- app/components/idea-item.js
+++ app/components/idea-item.js
@@ -9,19 +9,4 @@ export default class IdeaItemComponent extends Component {
   goToIdeaDetail() {
     this.router.transitionTo('ideas', this.args.idea.id);
   }
-
-  @action
-  delete() {
-    if (!confirm('Are you sure you want to delete this idea?')) {
-      return;
-    }
-
-    this.args.idea.destroyRecord().catch(error => {
-      console.error(error);
-      alert(
-        'An error occurred while trying to delete this idea.' +
-          ' Make sure there are no quotes associated with it and try again.',
-      );
-    });
-  }
 }
