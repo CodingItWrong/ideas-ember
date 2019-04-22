95518d642801774cbdf2733ca145593ba55bafa7
Josh Justice <me@codingitwrong.com>
2019-04-22 17:19:50 -0400

    Move author deleting from list to detail

--- app/components/author-header.js
+++ app/components/author-header.js
@@ -1,8 +1,11 @@
 import Component from '@glimmer/component';
 import { tracked } from '@glimmer/tracking';
 import { action } from '@ember/object';
+import { inject as service } from '@ember/service';
 
 export default class AuthorHeaderComponent extends Component {
+  @service router;
+
   @tracked
   editing = false;
 
@@ -26,5 +29,23 @@ export default class AuthorHeaderComponent extends Component {
     author.set('name', this.editedAuthorName);
     await author.save();
     this.editing = false;
+  }
+
+  @action
+  delete() {
+    if (!confirm('Are you sure you want to delete this author?')) {
+      return;
+    }
+
+    this.args.author
+      .destroyRecord()
+      .then(() => this.router.transitionTo('authors'))
+      .catch(error => {
+        console.error(error);
+        alert(
+          'An error occurred while trying to delete this author.' +
+            ' Make sure there are no sources associated with her/him and try again.',
+        );
+      });
   }
 }
