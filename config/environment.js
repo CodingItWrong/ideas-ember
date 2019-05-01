47270ec55e118582079c39aee3c4209728d37d2f
Josh Justice <me@codingitwrong.com>
2019-05-01 06:43:56 -0400

    Change production host

--- config/environment.js
+++ config/environment.js
@@ -51,7 +51,7 @@ module.exports = function(environment) {
 
   // eslint-disable-next-line no-empty
   if (environment === 'production') {
-    ENV.apiHost = 'https://ciw-ideas.herokuapp.com';
+    ENV.apiHost = 'https://api.ideas.codingitwrong.com';
   }
 
   return ENV;
