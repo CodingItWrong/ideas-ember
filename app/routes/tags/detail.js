81b610191cb5210563efcd72d2e767833a296456
Josh Justice <josh.justice@accesscfa.com>
2019-09-08 19:50:12 -0400

    Add source list to tags page

--- app/routes/tags/detail.js
+++ app/routes/tags/detail.js
@@ -2,6 +2,9 @@ import Route from '@ember/routing/route';
 
 export default class TagsDetailRoute extends Route {
   model({ id }) {
-    return this.store.findRecord('tag', id);
+    return this.store.findRecord('tag', id, {
+      reload: true,
+      include: 'sources',
+    });
   }
 }
