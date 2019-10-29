f9734626e702e9835ae89608fa08c4877bba6354
Josh Justice <josh.justice@accesscfa.com>
2019-10-29 07:17:03 -0400

    Sort sources newest to oldest

--- app/components/source-list.js
+++ app/components/source-list.js
@@ -2,7 +2,7 @@ import Component from '@glimmer/component';
 import { sort } from '@ember/object/computed';
 
 export default class SourceListComponent extends Component {
-  sortProperties = Object.freeze(['title:asc']);
+  sortProperties = Object.freeze(['createdAt:desc']);
 
   @sort('args.sources', 'sortProperties')
   sortedSources;
--- app/models/source.js
+++ app/models/source.js
@@ -5,6 +5,7 @@ export default class SourceModel extends Model {
   @attr() title;
   @attr() url;
   @attr() date;
+  @attr('date') createdAt;
   @belongsTo('author') author;
   @hasMany('quote') quotes;
   @hasMany('tag') tags;
