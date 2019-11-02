e8ec3a214a8cb8df8bd6808b15aa9a98d3a5d11e
Josh Justice <josh.justice@accesscfa.com>
2019-11-02 12:23:48 -0400

    Filter ideas and sources

--- app/components/idea-list.js
+++ app/components/idea-list.js
@@ -1,9 +1,20 @@
 import Component from '@glimmer/component';
-import { sort } from '@ember/object/computed';
+import { tracked } from '@glimmer/tracking';
+import { sort, filter } from '@ember/object/computed';
 
 export default class IdeaListComponent extends Component {
   sortProperties = Object.freeze(['createdAt:desc']);
 
+  @tracked filter = '';
+
   @sort('args.ideas', 'sortProperties')
   sortedIdeas;
+
+  @filter('sortedIdeas', ['filter'], function(idea) {
+    return (
+      !this.filter ||
+      idea.summary.toLowerCase().includes(this.filter.toLowerCase())
+    );
+  })
+  filteredIdeas;
 }
--- app/components/source-list.js
+++ app/components/source-list.js
@@ -1,9 +1,20 @@
 import Component from '@glimmer/component';
-import { sort } from '@ember/object/computed';
+import { tracked } from '@glimmer/tracking';
+import { sort, filter } from '@ember/object/computed';
 
 export default class SourceListComponent extends Component {
   sortProperties = Object.freeze(['createdAt:desc']);
 
+  @tracked filter = '';
+
   @sort('args.sources', 'sortProperties')
   sortedSources;
+
+  @filter('sortedSources', ['filter'], function(source) {
+    return (
+      !this.filter ||
+      source.title.toLowerCase().includes(this.filter.toLowerCase())
+    );
+  })
+  filteredSources;
 }
