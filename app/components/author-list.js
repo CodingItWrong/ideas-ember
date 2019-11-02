b3cdeb6764e3cdce3f15c55486c0d38459072ec8
Josh Justice <josh.justice@accesscfa.com>
2019-11-02 11:47:08 -0400

    WIP filter authors

--- app/components/author-list.js
+++ app/components/author-list.js
@@ -1,9 +1,20 @@
 import Component from '@glimmer/component';
-import { sort } from '@ember/object/computed';
+import { tracked } from '@glimmer/tracking';
+import { sort, filter } from '@ember/object/computed';
 
 export default class AuthorListComponent extends Component {
   sortProperties = Object.freeze(['name:asc']);
 
+  @tracked filter = '';
+
   @sort('args.authors', 'sortProperties')
   sortedAuthors;
+
+  @filter('sortedAuthors', ['filter'], function(author) {
+    return (
+      !this.filter ||
+      author.name.toLowerCase().includes(this.filter.toLowerCase())
+    );
+  })
+  filteredAuthors;
 }
