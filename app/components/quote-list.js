7b9bc03cdcc9aa8bae5dbca4dee61fab5664dea7
Josh Justice <josh.justice@accesscfa.com>
2020-02-04 19:52:31 -0500

    Natural sort for quotes in a source

--- app/components/quote-list.js
+++
@@ -1,9 +0,0 @@
-import Component from '@glimmer/component';
-import { sort } from '@ember/object/computed';
-
-export default class QuoteListComponent extends Component {
-  sortProperties = Object.freeze(['createdAt:asc']);
-
-  @sort('args.quotes', 'sortProperties')
-  sortedQuotes;
-}
