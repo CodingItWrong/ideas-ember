fc08b51022bd70da12f9c62ed59395ee32d29f3e
Josh Justice <josh.justice@accesscfa.com>
2020-05-18 07:59:18 -0400

    Change index route to a class route

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,11 +1,12 @@
 import Route from '@ember/routing/route';
+// eslint-disable-next-line ember/no-mixins
 import AuthenticatedRouteMixin from 'ember-simple-auth/mixins/authenticated-route-mixin';
 import { inject as service } from '@ember/service';
 
-export default Route.extend(AuthenticatedRouteMixin, {
-  session: service(),
+export default class IndexRoute extends Route.extend(AuthenticatedRouteMixin) {
+  @service session;
 
   model() {
     return this.store.findAll('idea');
-  },
-});
+  }
+}
