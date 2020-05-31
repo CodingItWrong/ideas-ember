565187471b9d6ae794d97984804b575d7b2c3f02
Josh Justice <josh.justice@accesscfa.com>
2020-05-31 13:44:24 -0400

    Group quote components

--- app/components/quote/edit-form.js
+++ app/components/quote/edit-form.js
@@ -2,7 +2,7 @@ import Component from '@glimmer/component';
 import { inject as service } from '@ember/service';
 import { tracked } from '@glimmer/tracking';
 import { action } from '@ember/object';
-import ENV from '../config/environment';
+import ENV from '../../config/environment';
 
 export default class EditQuoteForm extends Component {
   @service store;
