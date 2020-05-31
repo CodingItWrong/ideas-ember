879867c4fb88287216e46982f7842d4cc052c6a7
Josh Justice <josh.justice@accesscfa.com>
2020-05-31 13:47:15 -0400

    Group source components

--- app/components/source/header.js
+++ app/components/source/header.js
@@ -2,7 +2,7 @@ import Component from '@glimmer/component';
 import { tracked } from '@glimmer/tracking';
 import { action } from '@ember/object';
 import { inject as service } from '@ember/service';
-import ENV from '../config/environment';
+import ENV from '../../config/environment';
 
 export default class SourceHeaderComponent extends Component {
   @service router;
