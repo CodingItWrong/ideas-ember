4ad788fe11de120517d644f57074787517c1ed8c
Josh Justice <josh.justice@accesscfa.com>
2020-05-31 14:58:51 -0400

    Extract tag edit form

--- app/components/tag/header.js
+++ app/components/tag/header.js
@@ -6,46 +6,25 @@ import { inject as service } from '@ember/service';
 export default class TagHeader extends Component {
   @service router;
 
-  @tracked
-  editing = false;
-
-  @tracked
-  editedTagName;
+  @tracked editing = false;
 
   @action
   startEditing() {
-    this.editedTagName = this.args.tag.name;
     this.editing = true;
   }
 
   @action
-  cancelEditing() {
+  handleCancel() {
     this.editing = false;
   }
 
   @action
-  async save() {
-    const { tag } = this.args;
-    tag.set('name', this.editedTagName);
-    await tag.save();
+  handleSave() {
     this.editing = false;
   }
 
   @action
-  delete() {
-    if (!confirm('Are you sure you want to delete this tag?')) {
-      return;
-    }
-
-    this.args.tag
-      .destroyRecord()
-      .then(() => this.router.transitionTo('tags'))
-      .catch(error => {
-        console.error(error);
-        alert(
-          'An error occurred while trying to delete this tag.' +
-            ' Make sure there are no sources tagged with it and try again.',
-        );
-      });
+  handleDelete() {
+    this.router.transitionTo('tags');
   }
 }
