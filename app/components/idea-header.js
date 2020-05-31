a893a00a3b50867f0ba9a277436e5f6997fc9e3c
Josh Justice <josh.justice@accesscfa.com>
2020-05-31 11:59:59 -0400

    Extract edit idea form

--- app/components/idea-header.js
+++ app/components/idea-header.js
@@ -9,52 +9,23 @@ export default class IdeaHeaderComponent extends Component {
   @tracked
   editing = false;
 
-  @tracked
-  editedIdeaSummary;
-
-  @tracked
-  editedIdeaComments;
-
   @action
   startEditing() {
-    this.editedIdeaSummary = this.args.idea.summary;
-    this.editedIdeaComments = this.args.idea.comments;
     this.editing = true;
   }
 
   @action
-  cancelEditing() {
+  handleCancel() {
     this.editing = false;
   }
 
   @action
-  async save() {
-    const { idea } = this.args;
-    idea.setProperties({
-      summary: this.editedIdeaSummary,
-      comments: this.editedIdeaComments,
-    });
-    await idea.save();
+  handleSave() {
     this.editing = false;
   }
 
   @action
-  delete() {
-    if (!confirm('Are you sure you want to delete this idea?')) {
-      return;
-    }
-
-    this.args.idea
-      .destroyRecord()
-      .then(() => {
-        this.router.transitionTo('index');
-      })
-      .catch(error => {
-        console.error(error);
-        alert(
-          'An error occurred while trying to delete this idea.' +
-            ' Make sure there are no quotes associated with it and try again.',
-        );
-      });
+  handleDelete() {
+    this.router.transitionTo('index');
   }
 }
