b3ce34499e0274d1fc6a075cc5b2bc437556aca9
Josh Justice <me@codingitwrong.com>
2020-12-18 08:35:34 -0500

    Disable eslint rule that we can't fix now

--- app/components/idea/list.js
+++ app/components/idea/list.js
@@ -1,5 +1,8 @@
 import Component from '@glimmer/component';
 import { tracked } from '@glimmer/tracking';
+
+// TODO: using a getter instead of the sort computed property results in the list not updating upon creation, failing the acceptance test
+// eslint-disable-next-line ember/no-computed-properties-in-native-classes
 import { sort } from '@ember/object/computed';
 
 export default class IdeaListComponent extends Component {
