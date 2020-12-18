d0bd04c0e82d4d60a6f30d62ff2cdf2d869cff9b
Josh Justice <me@codingitwrong.com>
2020-12-18 07:18:56 -0500

    Convert filter to getter

--- app/components/author/list.js
+++ app/components/author/list.js
@@ -1,6 +1,5 @@
 import Component from '@glimmer/component';
 import { tracked } from '@glimmer/tracking';
-import { filter } from '@ember/object/computed';
 import sortBy from 'lodash/sortBy';
 
 export default class AuthorListComponent extends Component {
@@ -10,11 +9,13 @@ export default class AuthorListComponent extends Component {
     return sortBy(this.args.authors.toArray(), ['name']);
   }
 
-  @filter('sortedAuthors', ['filter'], function (author) {
-    return (
-      !this.filter ||
-      author.name.toLowerCase().includes(this.filter.toLowerCase())
+  get filteredAuthors() {
+    if (!this.filter) {
+      return this.sortedAuthors;
+    }
+
+    return this.sortedAuthors.filter(author =>
+      author.name.toLowerCase().includes(this.filter.toLowerCase()),
     );
-  })
-  filteredAuthors;
+  }
 }
