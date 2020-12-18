ff59a377c268322b6a4c5b4402b7dde0facc2beb
Josh Justice <me@codingitwrong.com>
2020-12-18 07:49:48 -0500

    Convert idea filter to getter

--- app/components/idea/list.js
+++ app/components/idea/list.js
@@ -1,6 +1,6 @@
 import Component from '@glimmer/component';
 import { tracked } from '@glimmer/tracking';
-import { sort, filter } from '@ember/object/computed';
+import { sort } from '@ember/object/computed';
 
 export default class IdeaListComponent extends Component {
   sortProperties = Object.freeze(['createdAt:desc']);
@@ -10,11 +10,13 @@ export default class IdeaListComponent extends Component {
   @sort('args.ideas', 'sortProperties')
   sortedIdeas;
 
-  @filter('sortedIdeas', ['filter'], function (idea) {
-    return (
-      !this.filter ||
-      idea.summary.toLowerCase().includes(this.filter.toLowerCase())
+  get filteredIdeas() {
+    if (!this.filter) {
+      return this.sortedIdeas;
+    }
+
+    return this.sortedIdeas.filter(idea =>
+      idea.summary.toLowerCase().includes(this.filter.toLowerCase()),
     );
-  })
-  filteredIdeas;
+  }
 }
