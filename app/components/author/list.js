48e15acb15948ec996856fdfc724f58f10526b82
Josh Justice <me@codingitwrong.com>
2020-12-18 07:17:33 -0500

    Convert sort to lodash

--- app/components/author/list.js
+++ app/components/author/list.js
@@ -1,14 +1,14 @@
 import Component from '@glimmer/component';
 import { tracked } from '@glimmer/tracking';
-import { sort, filter } from '@ember/object/computed';
+import { filter } from '@ember/object/computed';
+import sortBy from 'lodash/sortBy';
 
 export default class AuthorListComponent extends Component {
-  sortProperties = Object.freeze(['name:asc']);
-
   @tracked filter = '';
 
-  @sort('args.authors', 'sortProperties')
-  sortedAuthors;
+  get sortedAuthors() {
+    return sortBy(this.args.authors.toArray(), ['name']);
+  }
 
   @filter('sortedAuthors', ['filter'], function (author) {
     return (
