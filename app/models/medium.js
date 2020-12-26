aefb32c5db007173b4edf667fa71496fb234a80a
Josh Justice <me@codingitwrong.com>
2020-12-25 20:26:49 -0500

    Add classic decorator to all models

--- app/models/medium.js
+++ app/models/medium.js
@@ -1,5 +1,7 @@
 import Model, { attr, hasMany } from '@ember-data/model';
+import classic from 'ember-classic-decorator';
 
+@classic
 export default class MediumModel extends Model {
   @attr() name;
 }
