11882028b5c0565a772c914c2333219ba8238c0f
Josh Justice <me@codingitwrong.com>
2020-12-25 20:27:03 -0500

    Mark computed getter

--- app/models/idea.js
+++ app/models/idea.js
@@ -1,5 +1,6 @@
 import Model, { attr, hasMany } from '@ember-data/model';
 import classic from 'ember-classic-decorator';
+import { computed } from '@ember/object';
 
 @classic
 export default class IdeaModel extends Model {
@@ -8,6 +9,7 @@ export default class IdeaModel extends Model {
   @attr('date') createdAt;
   @hasMany('quote') quotes;
 
+  @computed('id')
   get numericId() {
     return Number(this.id);
   }
