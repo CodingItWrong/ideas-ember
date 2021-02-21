10d55cf870b95e18f081e5cb60712831c7d8aebc
Josh Justice <me@codingitwrong.com>
2021-02-21 08:22:28 -0500

    Fix lint errors

--- app/models/quote.js
+++ app/models/quote.js
@@ -1,4 +1,4 @@
-import Model, { attr, hasMany, belongsTo } from '@ember-data/model';
+import Model, { attr, belongsTo } from '@ember-data/model';
 import classic from 'ember-classic-decorator';
 
 @classic
--- tests/acceptance/administer-ideas-test.js
+++ tests/acceptance/administer-ideas-test.js
@@ -1,5 +1,5 @@
 import { module, test } from 'qunit';
-import { visit, click, fillIn, pauseTest } from '@ember/test-helpers';
+import { visit, click, fillIn } from '@ember/test-helpers';
 import { setupApplicationTest } from 'ember-qunit';
 import setupMirage from 'ember-cli-mirage/test-support/setup-mirage';
 import { authenticateSession } from 'ember-simple-auth/test-support';
