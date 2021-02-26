1ed79d891da3008b8d2085d35592686b3c1bbae3
Josh Justice <me@codingitwrong.com>
2021-02-26 17:53:18 -0500

    Update layout to BS

--- app/components/layout-container.js
+++ app/components/layout-container.js
@@ -1,6 +1,15 @@
 import Component from '@glimmer/component';
 import { tracked } from '@glimmer/tracking';
+import { action } from '@ember/object';
 
 export default class LayoutContainerComponent extends Component {
   @tracked sideNavOpen = false;
+
+  @action toggleSideNavOpen() {
+    this.sideNavOpen = !this.sideNavOpen;
+  }
+
+  @action hideSideNav() {
+    this.sideNavOpen = false;
+  }
 }
